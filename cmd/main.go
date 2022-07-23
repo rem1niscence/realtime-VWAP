24c512f9de2434368d5827f6eb39c4dfcbdb6107
Roniel Valdez <roniel_valdez@outlook.com>
2022-07-23 17:07:14 -0400

    feat: basic local cmd

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/rem1niscence/realtime-VWAP/calculator"
 	"github.com/rem1niscence/realtime-VWAP/subscription"
 )
 
@@ -14,7 +15,9 @@ func main() {
 		log.Fatalf("error de todo: %s", err.Error())
 	}
 
-	for match := range matches {
-		fmt.Printf("%+v\n", match)
+	vwaps := calculator.StreamPairsVWAP(matches, 200)
+
+	for match := range vwaps {
+		fmt.Printf("pair: %s | vwap: %.3f\n", match.Pair, match.VWAP)
 	}
 }
