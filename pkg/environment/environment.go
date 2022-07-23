511d67354a072b7395ec58fcf9da72d14a8e4064
Roniel Valdez <roniel_valdez@outlook.com>
2022-07-23 19:27:27 -0400

    feat: add env helpers

---
+++ pkg/environment/environment.go
@@ -0,0 +1,31 @@
+package environment
+
+import (
+	"os"
+	"strconv"
+)
+
+// GetInt64 gets the env var as an int
+func GetInt64(varName string, defaultValue int64) int64 {
+	val, ok := os.LookupEnv(varName)
+	if !ok {
+		return defaultValue
+	}
+
+	iVal, err := strconv.ParseInt(val, 10, 64)
+	if err != nil {
+		return defaultValue
+	}
+
+	return iVal
+}
+
+// GetString gets the environment var as a string
+func GetString(varName string, defaultValue string) string {
+	val, _ := os.LookupEnv(varName)
+	if val == "" {
+		return defaultValue
+	}
+
+	return val
+}
