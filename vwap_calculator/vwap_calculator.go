5d87017177090094ec35b8e7e5bd4bd52a02bd21
Roniel Valdez <roniel_valdez@outlook.com>
2022-07-24 14:10:18 -0400

    lint: add  linter fixes

--- vwap_calculator/vwap_calculator.go
+++ vwap_calculator/vwap_calculator.go
@@ -6,20 +6,23 @@ import (
 	"github.com/rem1niscence/realtime-VWAP/subscription"
 )
 
+// Calculator holds all the operations related to streaming Pair's VWAP
 type Calculator interface {
 	StreamPairsVWAP(matches <-chan subscription.Match, dataPoints int) <-chan *VWAP
 }
 
 var (
-	// ErrInvalidLimit when a limit under 1 is given
+	// ErrInvalidLimit when a limit under 1 is given.
 	ErrInvalidLimit = errors.New("invalid limit value. Must be at least 1 or more")
 )
 
+// Trade represents a trading operation.
 type Trade struct {
 	Quantity float32
 	Price    float32
 }
 
+// PairVWAP holds the trades needed to calculate the VWAP of a pair.
 type PairVWAP struct {
 	trades                     []*Trade
 	limit                      int
@@ -28,11 +31,13 @@ type PairVWAP struct {
 	vWAP                       float32
 }
 
+// VWAP is a helper struct to represent a pair-vwap mapping.
 type VWAP struct {
 	Pair string
 	VWAP float32
 }
 
+// NewPairVWAP returns a PairVWAP asserting it has valid values.
 func NewPairVWAP(limit int) (*PairVWAP, error) {
 	if limit <= 0 {
 		return nil, ErrInvalidLimit
@@ -43,17 +48,19 @@ func NewPairVWAP(limit int) (*PairVWAP, error) {
 	}, nil
 }
 
-// calculateVWAP calculates, saves and retrieves the vwap value of a pair
+// calculateVWAP calculates, saves and retrieves the vwap value of a pair.
 func (pr *PairVWAP) calculateVWAP() float32 {
 	pr.vWAP = pr.totalWeightedQuantityPrice / pr.totalQuantity
 	return pr.vWAP
 }
 
-// Returns VWAP for a trading pair
+// GetVWAP Returns VWAP for a trading pair
 func (pr *PairVWAP) GetVWAP() float32 {
 	return pr.vWAP
 }
 
+// AggregateTrade adds a Trade to a pair and calculates the VWAP, dropping oldest
+// values when it exceedes the window limit.
 func (pr *PairVWAP) AggregateTrade(pair string, trade *Trade) float32 {
 	if len(pr.trades) >= pr.limit {
 		oldestPairQuantity := pr.trades[0].Quantity
@@ -76,6 +83,8 @@ func (pr *PairVWAP) AggregateTrade(pair string, trade *Trade) float32 {
 	return pr.calculateVWAP()
 }
 
+// StreamPairsVWAP calculates VWAP for pairs given through the matches channel and
+// subsequently streams them to a listener.
 func StreamPairsVWAP(matches <-chan subscription.Match, dataPoints int) (<-chan *VWAP, error) {
 	vwapStream := make(chan *VWAP)
 	tradingPairs := map[string]*PairVWAP{}
