465db58e04095ec9004eec7a96158faaddb8b211
Roniel Valdez <roniel_valdez@outlook.com>
2022-07-23 20:33:43 -0400

    tests: add environment tests

---
+++ pkg/environment/environment_test.go
@@ -0,0 +1,71 @@
+package environment
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetStringDefaultValue(t *testing.T) {
+	t.Parallel()
+	c := require.New(t)
+
+	defaultValue := "val"
+	c.Equal(defaultValue, GetString("GET_STRING", defaultValue))
+}
+
+func TestGetStringCustomValue(t *testing.T) {
+	t.Parallel()
+	c := require.New(t)
+
+	withTestEnv("custom", func(varName string) {
+		c.Equal("custom", GetString(varName, ""))
+	})
+}
+
+func TestGetInt64DefaultValue(t *testing.T) {
+	t.Parallel()
+	c := require.New(t)
+
+	defaultValue := int64(872)
+	c.Equal(defaultValue, GetInt64("GET_INT64", defaultValue))
+}
+
+func TestGetInt64CustomValue(t *testing.T) {
+	t.Parallel()
+	c := require.New(t)
+
+	ttable := []struct {
+		val        interface{}
+		defaultVal int64
+		expected   int64
+	}{
+		{false, -1, -1},
+		{"34", -1, 34},
+		{-34, -1, -34},
+		{"00001", -1, 1},
+		{"x", -1, -1},
+		{0x7F, -1, 127},
+	}
+
+	for _, test := range ttable {
+		withTestEnv(test.val, func(varName string) {
+			c.Equal(test.expected, GetInt64(varName, test.defaultVal), "Failed to get %#v with %#v", varName, test.val)
+		})
+	}
+}
+
+func withTestEnv(val interface{}, codeBlock func(varName string)) {
+	varName := fmt.Sprintf("TEST_%d_%d", rand.Intn(math.MaxInt32), rand.Intn(math.MaxInt32))
+	_ = os.Setenv(varName, fmt.Sprintf("%v", val))
+
+	defer func() {
+		_ = os.Unsetenv(varName)
+	}()
+
+	codeBlock(varName)
+}
